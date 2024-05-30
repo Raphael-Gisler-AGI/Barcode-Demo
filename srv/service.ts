e111f4a71dedf6944a786b1539d85474d71ccbdd
Michael Galambos <michael.galambos@agilita.ch>
2024-05-30 11:09:38 +0200

    adds ts configuration and a solution to filter out items if UECHA eq 000000

---
+++ srv/service.ts
@@ -0,0 +1,23 @@
+import cds from "@sap/cds";
+
+class consignmentReturnsService extends cds.ApplicationService {
+  async init() {
+    const { Items } = this.entities;
+
+    this.on("READ", Items, async (req:cds.Request, next:any) => {
+      const res = await next();
+      if (Array.isArray(res)) {
+        if (req.params.length !== 0) return res
+        for (let i = res.length - 1; i > 0; i--) {
+          if (res[i]["UECHA"] !== "000000") {
+            res.splice(i, 1);
+          }
+        }
+      } else {
+        return res;
+      }
+    });
+    return super.init();
+  }
+}
+module.exports = consignmentReturnsService;
